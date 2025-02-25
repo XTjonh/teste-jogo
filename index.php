13130e07a7206abc1619d812cace6d491ce9ff3a
XTjonh <ljonh960@gmail.com>
2025-02-25 11:20:27 -0300

    feat

--- index.php
+++ index.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
     <title>Document</title>
 </head>
 <body>
+=======
+    <title>sistema</title>
+</head>
+<body>
+
+    <div>
+        
+    </div>
+>>>>>>> feature
     
 </body>
 </html>
